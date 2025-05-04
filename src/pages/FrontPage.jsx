abae8c459952ce03a3376460618e2953e6989360
Suhaib Rashid <suhaibrashid2003@gmail.com>
2025-05-04 20:29:09 +0500

    frontpage added

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -650,23 +650,7 @@ const MeetingMindFrontPage = () => {
     
     window.addEventListener('resize', handleResize);
     
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      mountRef.current.removeChild(renderer.domElement);
-      
-      scene.traverse((object) => {
-        if (object.geometry) object.geometry.dispose();
-        if (object.material) {
-          if (Array.isArray(object.material)) {
-            object.material.forEach(material => material.dispose());
-          } else {
-            object.material.dispose();
-          }
-        }
-      });
-      
-      renderer.dispose();
-    };
+ 
   }, [currentSection, scrollY]);
   
   const scrollToSection = (sectionRef) => {
